0eec0ea719f6b1751a6cd7283998d22e39d34080
Obai Alnajjar <obai@outlook.com>
2018-12-14 20:33:13 +0300

    Added password hashing and verification

--- itemsCatalogDB_setup.py
+++ itemsCatalogDB_setup.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, func, crea
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import current_timestamp
+from passlib.apps import custom_app_context as pwd_context
+
 Base = declarative_base()
 
 
@@ -19,9 +21,15 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     full_name = Column(String(80), nullable=False)
-    email = Column(String(120), nullable=False)
-    password_hash = Column(String(64))
+    email = Column(String(120), index=True)
+    password_hash = Column(String(64), nullable=False)
     image = Column(String(250))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
 
 class Item(Base):
