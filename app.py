d75033ff0797fdbac67800bc857db99f56bb4fd2
Obai Alnajjar <obai@outlook.com>
2018-12-14 18:20:11 +0300

    Added DB connection

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, make_response
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from itemsCatalogDB_setup import Base, User, Item, Category
+import random, string, httplib2, json,requests
 app = Flask(__name__)
 
+# Connect to Database and create database session
+engine = create_engine('sqlite:///itemsCatalog.db?check_same_thread=False')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def hello_world():
@@ -9,4 +18,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
