b1b19269c1dc155ced4d40c209d5b91e36101595
Obai Alnajjar <obai@outlook.com>
2018-12-14 17:40:23 +0300

    Added category model to DB

--- itemsCatalogDB_setup.py
+++ itemsCatalogDB_setup.py
@@ -8,6 +8,14 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+
 
 
 engine = create_engine('sqlite:///itemsCatalog.db')
