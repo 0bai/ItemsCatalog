f3d38e57b619655fb9f90ccec965eb249177a2ef
Obai Alnajjar <obai@outlook.com>
2018-12-15 15:51:30 +0300

    Added edit item route

--- app.py
+++ app.py
@@ -22,26 +22,56 @@ def show_landing_page():
     return render_template("index.html", categories=categories, latest=latest)
 
 
+# Show category page route
 @app.route("/<string:category>")
 def show_category(category):
-    category_id = session.query(Category).filter_by(name=category).one().id
+    category_id = (session.query(Category).filter_by(name=category).one()).id
     categories = session.query(Category).all()
     items = session.query(Item).filter_by(category_id=category_id).all()
     return render_template("category/show.html", category=category, items=items, categories=categories)
 
 
+# Add new item page route
 @app.route("/items/new", methods=["GET", "POST"])
 def new_item():
     if request.method == "GET":
         return render_template("item/new.html")
     category = session.query(Category).filter_by(id=request.form["category"]).one()
-    #take the user from the login session
+    # take the user from the login session
     user = session.query(User).filter_by(id=1).one()
-    item = Item(title=request.form["title"],  description=request.form["description"], image=request.form["image"], category=category, user=user)
+    item = Item(title=request.form["title"], description=request.form["description"], image=request.form["image"],
+                category=category, user=user)
     session.add(item)
     session.commit()
     return redirect(url_for("show_category", category=category.name))
 
+
+# Edit item page route
+@app.route("/<string:category>/<string:item>/edit", methods=["GET", "POST"])
+def edit_item(category, item):
+    category = session.query(Category).filter_by(name=category).one()
+    item = session.query(Item).filter_by(category=category, title=item).one()
+    categories = session.query(Category).all()
+    if item:
+        if request.method == "GET":
+            return render_template("item/edit.html", item=item, category=category, categories=categories)
+        if request.form["category"] != category.name:
+            new_category = session.query(Category).filter_by(name=request.form["category"]).one()
+            if session.query(Item).filter_by(title=item.title, category=new_category).count():
+                # Flash error message item already exists
+                return redirect(url_for("edit_item", item=item.title, category=category.name))
+            category = new_category
+        item.title = request.form["title"]
+        item.description = request.form["description"]
+        item.image = request.form["image"]
+        item.category = category
+        session.add(item)
+        session.commit()
+        return redirect(url_for("show_item", item=item.title, category=category.name))
+    return redirect(url_for("show_landing_page"))
+
+
+# Show item page route
 @app.route("/<string:category>/<string:item>")
 def show_item(category, item):
     category = session.query(Category).filter_by(name=category).one()
