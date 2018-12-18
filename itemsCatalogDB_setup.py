5e684d106c533b406513d48710764a5e8b385361
Obai Alnajjar <obai@outlook.com>
2018-12-18 03:30:18 +0300

    removed password from user model & our own implemented authentication to adhere to project specifications

--- itemsCatalogDB_setup.py
+++ itemsCatalogDB_setup.py
@@ -22,14 +22,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     email = Column(String(120), index=True)
-    password_hash = Column(String(64))
     image = Column(String(250))
-
-    def hash_password(self, password):
-        self.password_hash = pwd_context.hash(password)
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
 
 
 class Item(Base):
